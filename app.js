9640a84b46fdfce1ac3634ead65e77aa49424661
ギッチ <34914112+Gitschi@users.noreply.github.com>
2018-07-14 18:55:28 +0900

    fix arguments for retweet method
    - started using tweet.id_str instead of tweet.id
    - added more relevant tags

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ var secret = require("./secret"); // Imports API keys
 var T = new Twit(secret); // Pushes API keys into constructor and makes new object
 
 // Looks for certain hashtags in the stream
-var stream = T.stream('statuses/filter', { track: ['#vrgamers', '#vrgames', '#vrchat', '#beatsaber'], language: 'en'});
+var stream = T.stream('statuses/filter', { track: ['#vrgamers', '#vrgaming', '#vrgame', '#vrgames', '#vrchat', '#beatsaber'], language: 'en'});
 
 // retweets found tweets that match the criteria
 stream.on('tweet', function (tweet) {
   if(isNotRt(tweet)){
-    T.post('statuses/retweet/:id', { id: tweet.id }, function (err, data, response) {
+    T.post('statuses/retweet/:id', { id: tweet.id_str }, function (err, data, response) {
       console.log(data)
     }) 
   }
