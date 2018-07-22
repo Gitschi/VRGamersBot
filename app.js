fd27e4bb113f436d4672dd368e62757972436344
ギッチ <34914112+Gitschi@users.noreply.github.com>
2018-07-22 21:00:32 +0900

    add blacklist
    - Added blacklist to filter irrelevant posts or spam

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var Twit = require('twit');
 var env = require("./env"); // Imports API keys
+var blacklist = require("./blacklist"); // Imports blacklist
 var T = new Twit(env.keys); 
 var maxHashtags = 7; // Sets max amount of Hashtags for spam check
 
@@ -18,7 +19,7 @@ stream.on('tweet', function (tweet) {
         console.log(err)
       }
       else{
-        console.log("Retweeted " + tweet.user.name + "'s post with id: " + tweet.id_str)
+        console.log("Retweeted " + tweet.user.screen_name + "'s post with id: " + tweet.id_str)
       }
     }) 
     T.post('favorites/create', id, function (err, data, response) {
@@ -31,7 +32,7 @@ stream.on('tweet', function (tweet) {
 
 // Runs tweet through checks
 function checkTweet(tweet){
- if(checkOriginality(tweet) && checkSpam(tweet) && checkInsta(tweet)){
+ if(checkOriginality(tweet) && checkSpam(tweet) && checkInsta(tweet) && checkBlacklist(tweet)){
    return true;
  }
  else{
@@ -46,7 +47,7 @@ function checkOriginality(tweet){
     isOriginal = true;
   }
   else{
-    console.log(tweet.user.name + "'s tweet with id: " + tweet.id_str + " is not original.")
+    console.log(tweet.user.screen_name + "'s tweet with id: " + tweet.id_str + " is not original.")
   }
   return isOriginal;
 }
@@ -55,11 +56,11 @@ function checkOriginality(tweet){
 function checkSpam(tweet){
   var isNotSpam = false;
   if(tweet.entities.hashtags.length <= maxHashtags){
-    console.log("hashtags: " + tweet.entities.hashtags.length + "vs. " + maxHashtags);
+    console.log("hashtags: " + tweet.entities.hashtags.length + " of maximum: " + maxHashtags);
     isNotSpam = true;
   }
   else{
-    console.log(tweet.user.name + "'s tweet with id: " + tweet.id_str + " is not most likely spam.")
+    console.log(tweet.user.screen_name + "'s tweet with id: " + tweet.id_str + " is most likely spam.")
   }
   return isNotSpam;
 }
@@ -69,10 +70,22 @@ function checkInsta(tweet){
   var noInstaLink = true;
   for(let i = 0; i < tweet.entities.urls.length; i++){
     console.log(tweet.entities.urls[i].expanded_url.slice(0, 24));
-    if(tweet.entities.urls[i].expanded_url.slice(0, 24) === "https://www.instagram.com"){
+    if(tweet.entities.urls[i].expanded_url.slice(0, 20) === "https://instagram.com"){
       noInstaLink = false;
       console.log(tweet.user.name + "'s tweet with id: " + tweet.id_str + " contains an instagram link.")
     }  
   }
   return noInstaLink;
+}
+
+// Checks that the user is not blacklisted
+function checkBlacklist(tweet){
+  var isNotBlacklisted = true;
+  for(let i = 0; i < blacklist.names.length; i++){
+    if(tweet.user.screen_name === blacklist.names[i]){
+      isNotBlacklisted = false;
+      console.log("User: " + tweet.user.screen_name + " is blacklisted.");
+    }
+  }
+  return isNotBlacklisted;
 }
---
+++ blacklist.js
@@ -0,0 +1,6 @@
+/* I mean no offense to the people on here, but I want to keep the feed relevant
+ to VR Gamers and the hashtags might pick up stuff that is not. */
+
+module.exports = {
+  names: ["toe_fur"]
+};
