efed23131af5a4368f6339642e9a7fd2306626e3
ギッチ <34914112+Gitschi@users.noreply.github.com>
2018-08-18 01:08:50 +0900

    Update blacklist
    Added two users to blacklist for burst posting

--- blacklist.js
+++ blacklist.js
@@ -1,9 +1,10 @@
 /* I mean no offense to the people on here, but I want to keep the feed relevant
- to VR Gamers and the hashtags might pick up stuff that is not. */
+ to VR Gamers and the hashtags might pick up stuff that is not.
+ Also want to prevent burst posts from a single user. */
 
 module.exports = {
   names: [
     'toe_fur', 'DeviantTrinity', 'PollyTalks1', 'terpon_sa', 'Indiandante', 'iconohash', 'shanghai_game',
-    'jackjjackman', 'mailorderprofit'
+    'jackjjackman', 'mailorderprofit', 'jsfey', 'Sd_Flandre'
   ]
 };
